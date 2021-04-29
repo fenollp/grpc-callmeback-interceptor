e093edc9677a1ccdcdf3d9308f86a6fcb1455e25
Pierre Fenoll <pierrefenoll@gmail.com>
2021-04-29 13:49:45 +0200

    fmt
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- go-callmeback/buffconn/buffconn_test.go
+++ go-callmeback/buffconn/buffconn_test.go
@@ -1,32 +1,32 @@
 package buffconn
 
 import (
-    "context"
-    "testing"
-    "time"
+	"context"
+	"testing"
+	"time"
 
-    "github.com/fenollp/grpc-callmeback-interceptor/go-callmeback"
-    "github.com/stretchr/testify/require"
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/metadata"
+	"github.com/fenollp/grpc-callmeback-interceptor/go-callmeback"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 func TestUnaryServerInterceptor_CallMeBackPass(t *testing.T) {
-    ctx := context.Background()
-    conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
-    require.NoError(t, err)
-    defer conn.Close()
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
+	require.NoError(t, err)
+	defer conn.Close()
 
-    client := NewEchoClient(conn)
-    const msg = "Hello Joe"
+	client := NewEchoClient(conn)
+	const msg = "Hello Joe"
 
-    var trailer metadata.MD
-    req := &EchoRequest{Message: msg}
-    rep, err := client.UnaryEcho(ctx, req, grpc.Trailer(&trailer))
-    require.NoError(t, err)
-    require.Equal(t, rep.Message, msg)
+	var trailer metadata.MD
+	req := &EchoRequest{Message: msg}
+	rep, err := client.UnaryEcho(ctx, req, grpc.Trailer(&trailer))
+	require.NoError(t, err)
+	require.Equal(t, rep.Message, msg)
 
-    pauseFor, err := callmeback.In(trailer)
-    require.NoError(t, err)
-    require.Equal(t, pauseFor, 1*time.Second)
+	pauseFor, err := callmeback.In(trailer)
+	require.NoError(t, err)
+	require.Equal(t, pauseFor, 1*time.Second)
 }
